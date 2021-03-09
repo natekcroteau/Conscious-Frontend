b455ae98a59c6cc30b40b8b00e6a5aaddcead061
natekcroteau <natekcroteau@gmail.com>
2021-03-09 10:22:01 -0700

    added rich text editor component to freewrite component

--- src/components/FreewriteContainer.js
+++ src/components/FreewriteContainer.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './FreewriteContainer.css'
 import CalendarContainer from './CalendarContainer'
+import FreewriteTextInput from './FreewriteTextInput'
 
 
 export default function FreewriteContainer() {
   return (
     <div className={"freewrite-container"}>
       <CalendarContainer />
-      
+      <FreewriteTextInput />
     </div>
   )
 }
--- src/components/GratitudeForm.js
+++ src/components/GratitudeForm.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function  () {
+export default function GratitudeForm() {
   return (
     <div>
       
     </div>
   )
 }
+
